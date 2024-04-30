9f7a7aa48b9a012327842c52699a46e7275436e2
hu8813 <huaydin@student.42vienna.com>
2024-04-30 23:14:36 +0200

    a

--- 0/ex03/NULL_not_found.py
+++ 0/ex03/NULL_not_found.py
@@ -1,6 +1,13 @@
 def NULL_not_found(object: any) -> int:
 	try:
-	
-	
+		if type(object):
+			if isinstance(object, (str, int, bool, float)):
+				print(object,":", type(object).__name__.title(),":",type(object))
+			else:
+				print("Type not found")
+		else:
+			print("Type not found")
 	except Exception as e:
 		print("An error occured " + str(e))
+		
+	return 1
